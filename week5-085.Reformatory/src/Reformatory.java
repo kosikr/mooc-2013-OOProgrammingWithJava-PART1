58d6cd89ae6491db52805354cb2ed05c26899218
Rafal <rafal.kos@yahoo.com>
2018-11-13 16:21:18 +0100

    added solution to week5-085

--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,21 @@
+
 public class Reformatory {
+
+    private int measuredTimes;
 
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        measuredTimes++;
+        return person.getWeight();
     }
 
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+
+    }
+
+    public int totalWeightsMeasured() {
+        return measuredTimes;
+
+    }
 }
