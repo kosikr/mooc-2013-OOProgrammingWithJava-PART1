17d6889a8d31fda655fb5a70d6920807802bd074
Rafal <rafal.kos@yahoo.com>
2018-11-25 15:08:37 +0100

    Solution

--- week6-101.Library/src/Library.java
+++ week6-101.Library/src/Library.java
@@ -11,22 +11,51 @@
 import java.util.ArrayList;
 
 public class Library {
-
+    
     private ArrayList<Book> booksList;
-
+    
     public Library() {
         this.booksList = new ArrayList<Book>();
     }
-
+    
     public void addBook(Book newBook) {
         this.booksList.add(newBook);
-
+        
     }
-
+    
     public void printBooks() {
         for (Book printing : this.booksList) {
             System.out.println(printing);
         }
     }
-
+    
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> foundTitle = new ArrayList<Book>();
+        for (Book ifContain : this.booksList) {
+            if (StringUtils.included(ifContain.title(), title)) {
+                foundTitle.add(ifContain);
+            }
+        }
+        return foundTitle;
+    }
+    
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> foundPublisher = new ArrayList<Book>();
+        for (Book ifContain : this.booksList) {
+            if (StringUtils.included(ifContain.publisher(), publisher)) {
+                foundPublisher.add(ifContain);
+            }
+        }
+        return foundPublisher;
+    }
+    
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> foundYear = new ArrayList<Book>();
+        for (Book ifContain : this.booksList) {
+            if (ifContain.year() == year) {
+                foundYear.add(ifContain);
+            }
+        }
+        return foundYear;
+    }
 }
